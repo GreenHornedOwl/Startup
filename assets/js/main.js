f8ccb9866553b64f59137838248d2300ba1f4b19
Daniel Voicu <daniel.voicu@brandfusion.ro>
2015-03-15 21:07:58 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
+    
+
 	$(".home-slider .owl-carousel").owlCarousel({
     loop:true,  
-    nav:true,
+    nav:false,
     dots: true,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
     responsive:{
         480:{
             items:1
@@ -14,6 +19,36 @@ $(document).ready(function(){
             items:1
         }
     }
-   }); 
+   });
+    $(".summerwear a").on("click",function(e){
+        e.preventDefault();
+        $(this).toggleClass("checked");
+    });
+    mobileFilter()
+    $(window).resize(function(){        
+        mobileFilter();
+    });
+  
+    function mobileFilter() { 
+        var width = $(window).width();
+        var open = false;
+            if (width < 768) {  
+                   $(".filter-vertical .filter-list").hide(); 
+                   $(".filter-header").on("click",function(){    
+                       if (!open ) {                             
+                                $(".filter-vertical .filter-list").stop(true,true).slideDown("slow");
+                                open = true;                                 
+                            
+                       } else {                           
+                            $(".filter-vertical .filter-list").stop(true,true).slideUp("slow");
+                            open = false;                           
+                        } 
+                    });                        
+            } else {
+                $(".filter-vertical .filter-list").show();
+                open = false;                
+            }
+    }
+    
 
 });
